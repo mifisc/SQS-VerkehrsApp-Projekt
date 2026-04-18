b7ab175685e324493f7ad7a6d9fb399399916d54
Louisa Böhm <lb@4boehm.de>
2026-04-18 17:46:22 +0200

    RQ-2 external Api Integration Tests ergänzt

---
+++ backend/sqs-verkehrsapp/src/test/java/de/th_ro/sqs_verkehrsapp/dto/wrapper/ClosureResponseTest.java
@@ -0,0 +1,41 @@
+package de.th_ro.sqs_verkehrsapp.dto.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ClosureResponseTest {
+
+    @Autowired
+    private JacksonTester<ClosureResponse> json;
+
+    @Test
+    void shouldDeserializeClosureResponse() throws Exception {
+        String content = """
+            {
+              "closure": [
+                {
+                  "identifier": "id-1",
+                  "title": "A1 | Test",
+                  "display_type": "WEIGHT_LIMIT_35",
+                  "coordinate": {
+                    "lat": "51.036440",
+                    "long": "6.964910"
+                  }
+                }
+              ]
+            }
+            """;
+
+        ClosureResponse response = json.parseObject(content);
+
+        assertThat(response.getClosures()).hasSize(1);
+        assertThat(response.getClosures().get(0).getDisplayType()).isEqualTo("WEIGHT_LIMIT_35");
+        assertThat(response.getClosures().get(0).getCoordinate().getLongValue()).isEqualTo("6.964910");
+    }
+
+}
---
+++ backend/sqs-verkehrsapp/src/test/java/de/th_ro/sqs_verkehrsapp/dto/wrapper/WarningResponseTest.java
@@ -0,0 +1,41 @@
+package de.th_ro.sqs_verkehrsapp.dto.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JsonTest
+class WarningResponseTest {
+
+    @Autowired
+    private JacksonTester<WarningResponse> json;
+
+    @Test
+    void shouldDeserializeWarningResponse() throws Exception {
+        String content = """
+            {
+              "warning": [
+                {
+                  "identifier": "id-1",
+                  "title": "A1 | Test",
+                  "display_type": "WEIGHT_LIMIT_35",
+                  "coordinate": {
+                    "lat": "51.036440",
+                    "long": "6.964910"
+                  }
+                }
+              ]
+            }
+            """;
+
+        WarningResponse response = json.parseObject(content);
+
+        assertThat(response.getWarnings()).hasSize(1);
+        assertThat(response.getWarnings().get(0).getDisplayType()).isEqualTo("WEIGHT_LIMIT_35");
+        assertThat(response.getWarnings().get(0).getCoordinate().getLongValue()).isEqualTo("6.964910");
+    }
+
+}
