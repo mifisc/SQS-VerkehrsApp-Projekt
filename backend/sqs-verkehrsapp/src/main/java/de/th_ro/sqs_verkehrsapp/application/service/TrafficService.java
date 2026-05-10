21983a2cf5fc6b771020288f71f782242398ca7e
Louisa Böhm <lb@4boehm.de>
2026-05-10 17:05:05 +0200

    Alle Meldungen der Autobahnen werden unter dem Endpunkt /api/traffic aufgelistet

--- backend/sqs-verkehrsapp/src/main/java/de/th_ro/sqs_verkehrsapp/application/service/TrafficService.java
+++ backend/sqs-verkehrsapp/src/main/java/de/th_ro/sqs_verkehrsapp/application/service/TrafficService.java
@@ -2,8 +2,12 @@ package de.th_ro.sqs_verkehrsapp.application.service;
 
 import de.th_ro.sqs_verkehrsapp.application.port.in.TrafficQueryUseCase;
 import de.th_ro.sqs_verkehrsapp.application.port.out.AutobahnApiPort;
+import de.th_ro.sqs_verkehrsapp.domain.model.RoadEvent;
 import de.th_ro.sqs_verkehrsapp.domain.model.TrafficEventsResult;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class TrafficService implements TrafficQueryUseCase {
@@ -17,5 +21,17 @@ public class TrafficService implements TrafficQueryUseCase {
     @Override
     public TrafficEventsResult getTrafficEvents(String roadId) {
         return autobahnApiPort.getTrafficEvents(roadId);
+    }
+
+    @Override
+    public TrafficEventsResult getAllTrafficEvents() {
+
+        List<String> roadIds = autobahnApiPort.getAvailableRoadIds();
+
+        List<RoadEvent> events = roadIds.stream()
+                .flatMap(roadId -> autobahnApiPort.getTrafficEvents(roadId).events().stream())
+                .toList();
+
+        return new TrafficEventsResult(events, true, LocalDateTime.now());
     }
 }
